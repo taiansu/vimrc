1c13802d181fc5ce2d0e09d0f6d45274457a1fc6
Tai An Su <taiansu@gmail.com>
2017-05-26 03:44:34 +0800

    Update README and install scripts

--- README.md
+++ README.md
@@ -1,8 +1,35 @@
 # taiansu's vimrc
 
-## ONE-STEP INSTALL
+## INSTALLATION
+
+### Have your own copy (Recommand)
+
+1. Fork this repo on Github
+2. Clone your repo into your computer by `git clone YOUR_REPO_LINK`
+3. `cd vimrc`
+4. Run `./local_install.sh`
+
+### One-step install
 
 `curl -o https://raw.githubusercontent.com/taiansu/vimrc/master/auto-install.sh | sh`
+
+## BASIC TUTORIAL
+
+* <leader> key
+  <space>
+
+  Or change it at vimrc's line 480 (or around)
+
+* Toggle NERDTree
+  <leader>q
+
+* Search file by Ctrlp
+  <option>p
+
+* Toggle tagbar
+  <leader>t
+
+Find other useful keys in vimrc, mostly after line 480
 
 ---
 
--- local_install.sh
+++ local_install.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 VIMHOME=~/.vim
-REPO_NAME=taiansu/vimrc
 LOCAL_NAME=vimrc
-REPO_PATH=$(printf "git://github.com/%s.git" $REPO_NAME)
 VIM_DIR=$(printf "%s/%s/vim" $(pwd) $LOCAL_NAME)
 VIMRC=$(printf "%s/%s/vimrc" $(pwd) $LOCAL_NAME)
 GVIMRC=$(printf "%s/%s/gvimrc" $(pwd) $LOCAL_NAME)
@@ -19,9 +17,6 @@ die() {
 [ -e "$VIMHOME" ] && die "$VIMHOME already exists."
 [ -e "~/.vimrc" ] && die "~/.vimrc already exists."
 
-# Clone the repo
-git clone $REPO_PATH $LOCAL_NAME
-
 # Link folder and files
 cd ~
 ln -s $VIM_DIR $VIMHOME
@@ -33,4 +28,4 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim -n -c "set nomore" +PlugUpgrade +PlugInstall +qall
 
-echo "Vim is ready to use. Happy Hacking～"
+echo "Your own vim is ready to use. Happy Hacking～"
