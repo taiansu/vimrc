a351a90abd312b332b358aeeed1f21f3db42d3b2
Tai An Su <taiansu@users.noreply.github.com>
2017-05-26 02:32:32 +0800

    Update auto_install.sh

--- auto_install.sh
+++ auto_install.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 VIMHOME=~/.vim
+REPO_NAME=taiansu/vimrc
+LOCAL_NAME=vimrc
+REPO_PATH=$(printf "git://github.com/%s.git %s" $REPO_NAME $LOCAL_NAME)
+VIM_DIR=$(printf "%s/%s/vim" $(pwd) $LOCAL_NAME)
+VIMRC=$(printf "%s/%s/vimrc" $(pwd) $LOCAL_NAME)
+GVIMRC=$(printf "%s/%s/gvimrc" $(pwd) $LOCAL_NAME)
 
 warn() {
     echo "$1" >&2
@@ -11,23 +17,20 @@ die() {
 }
 
 [ -e "$VIMHOME" ] && die "$VIMHOME already exists."
-[ -e "~/.vim" ] && die "~/.vim already exists."
 [ -e "~/.vimrc" ] && die "~/.vimrc already exists."
 
-# Change the repo
-REPO_NAME=taiansu/vim_tsu
+# Clone the repo
+git clone $REPO_PATH $LOCAL_NAME
 
-REPO_PATH=$(printf "git://github.com/%s.git" $REPO_NAME)
-git clone "$REPO_PATH" vim_repo
+# Link folder and files
+cd ~
+ln -s $VIM_DIR $VIMHOME
+ln -s $VIMRC .vimrc
+ln -s $GVIMRC .gvimrc
 
-VIM_DIR=$(printf "%s/vim_repo/vim" $(pwd))
-VIMRC=$(printf "%s/vim_repo/vimrc" $(pwd))
-
-cd
-ln -s "$VIM_DIR" "$VIMHOME"
-ln -s "$VIMRC" .vimrc
-
-cd "$VIMHOME"
+cd $VIMHOME
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim -n -c "set nomore" +PlugUpgrade +PlugInstall +qall
 
 echo "Your own vim is ready to use. Happy Hacking～"
